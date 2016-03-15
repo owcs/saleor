7e4854d328fc9df1abdb97d0a78f70fe3383411e
owcs <owcs@nerdattack.com.my>
2016-03-15 15:06:47 +0800

    Set default password hasher to BCryptSHA256PasswordHasher

--- saleor/settings.py
+++ saleor/settings.py
@@ -200,6 +200,16 @@ LOGGING = {
     }
 }
 
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.SHA1PasswordHasher',
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+    'django.contrib.auth.hashers.CryptPasswordHasher',
+]
+
 AUTHENTICATION_BACKENDS = (
     'saleor.registration.backends.EmailPasswordBackend',
     'saleor.registration.backends.ExternalLoginBackend',
